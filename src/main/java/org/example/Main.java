769f6e081fffc886e43e215889cf604f98bd3970
Lutuk145 <lukas.knej@gmail.com>
2022-11-25 22:02:41 +0100

    Made work input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,49 @@
 package org.example;
 
-import org.json.JSONObject;
-import org.json.JSONString;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.Writer;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        JSONObject j = new JSONObject();
-        JSONString j = new JSONString;
-
-        j.put("name", "Hukas Maj");
+    public static void main(String[] args) throws Exception {
 
 
-        FileWriter writer =new FileWriter("C:/Users/lukas/Desktop/IDKds/src/main/java/org/example/stats.json");
-        writer.write(j.);
+        JSONObject json = (JSONObject) readJSON(".\\jsonFiles\\stats.json");
+        String name = (String) json.get("name");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        name = input.nextLine();
+        json.put("name", name);
+        System.out.println(name);
+
+
+
+
+        FileWriter fileWriter = new FileWriter(".\\jsonFiles\\stats.json");
+        fileWriter.write(json.toJSONString());
+        fileWriter.flush();
+        fileWriter.close();
+
 
 
 
 
     }
+
+    public static Object readJSON(String fileName) throws Exception {
+        FileReader reader = new FileReader(fileName);
+        JSONParser parser = new JSONParser();
+        return parser.parse(reader);
+
+    }
 }
+
+
+
